192281c0840dbbe67989a0705a490ee800b04568
Mico <qmichael1234@gmail.com>
2019-07-26 14:25:11 +0800

    Section 3, Lecture 7: File uploaded to path

--- routes/company.js
+++ routes/company.js
@@ -1,7 +1,33 @@
+var formidable = require('formidable');
+var path = require('path');
+var fs = require('fs');
+
 
 module.exports = (app) => {
     app.get('/company/create', (req, res) => {
         res.render('company/company', {title: 'Company Registration', user: req.user});
     });
     
+    app.post('/upload', (req, res) => {
+        var form = new formidable.IncomingForm();
+
+        form.uploadDir =  path.join(__dirname, '../public/uploads');
+
+        form.on('file', (field, file) => {
+            fs.rename(file.path, path.join(form.uploadDir, file.name), (err) => {
+                if(err){
+                    throw err
+                }
+                console.log('File has been renamed');
+                
+            });
+        }),
+        form.on('error', (err) => {
+            console.log('An error occured', err);
+        }),
+        form.on('end', () => {
+            console.log('File upload was successful');
+        });
+        form.parse(req);
+    });
 }
