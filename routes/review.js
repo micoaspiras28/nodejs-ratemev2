55fb5efdf02bc74b8516c458d0472516b5b102d7
Mico <qmichael1234@gmail.com>
2019-07-27 17:38:54 +0800

    Section 3, Lecture 17: Star rating and Review JS

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,11 @@
 
+var Company = require('../models/company');
+
 module.exports = (app) => {
+
     app.get('/review/:id', (req, res) => {
-        res.render('company/review', {title: 'Company Review || Rate Me', user: req.user});
-    })
+        Company.findOne({'_id': req.params.id}, (err, data) => {
+            res.render('company/review', {title: 'Company Review || Rate Me', user: req.user, data: data});
+        })
+    });
 }
